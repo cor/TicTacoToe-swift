9548505cb16e5bd70e3ebc51ebaad500be8d4244
CorPruijs <cor@pruys.com>
2014-07-18 23:51:33 +0200

    Reverted to working version without the not working IBOutletCollection

--- TicTacoToe-swift/ViewController.swift
+++ TicTacoToe-swift/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //without collection
     @IBOutlet strong var levelField0: UIButton = UIButton()
     @IBOutlet strong var levelField1: UIButton = UIButton()
     @IBOutlet strong var levelField2: UIButton = UIButton()
@@ -20,32 +19,22 @@ class ViewController: UIViewController {
     @IBOutlet strong var levelField6: UIButton = UIButton()
     @IBOutlet strong var levelField7: UIButton = UIButton()
     @IBOutlet strong var levelField8: UIButton = UIButton()
-    @IBOutlet strong var currentPlayerLabel: UILabel
-    
-    
-    //the collection that causes the self.currentPlayerLabel not initalized at super.init call error
-    @IBOutlet var levelField: Array<UIButton>
-  
-    
-    var level = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    // can be 1 or 2
-    var currentPlayer = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        levelField[2].backgroundColor = UIColor.redColor()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func levelFieldPressed(sender: UIButton) {
+        println(sender.restorationIdentifier)
         if let buttonName = sender.restorationIdentifier? {
-            println(sender.restorationIdentifier! + " pressed")
             self.setValue(UIColor.redColor(), forKeyPath: "self.levelField\(Array(buttonName)[2]).backgroundColor")
         }
     }
+    
 }
